d52174428807d0dea4be50f1e681ed8fecbc1278
Nwafor6 <nwaforglory6@gmail.com>
2023-05-14 00:16:38 +0100

    added cloud domain to allowed host

--- reviewproj/settings.py
+++ reviewproj/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config("SECRET_KEY",default='^4-737n**(f#lssg_(7zav%dg7@k=$76!ry%zb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["swiftreview.pythonanywhere.com"]
 
 
 # Application definition
