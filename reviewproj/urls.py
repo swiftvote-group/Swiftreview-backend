19d392f40dfeadf785417cec538dd3ff35f0ccc0
Nwafor6 <nwaforglory6@gmail.com>
2023-05-14 01:04:08 +0100

    fixing cloud static bug

--- reviewproj/urls.py
+++ reviewproj/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path("v1/", include("mainapp.urls"),)
 ]
 if settings.DEBUG:
+    urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
